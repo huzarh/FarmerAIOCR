5b53a186f35538231ce13aae5fa218c377bfcca5
Khuzair <huzarh44@gmail.com>
2025-11-16 16:44:22 +0300

    orc

--- ocr_easyocr.py
+++ ocr_easyocr.py
@@ -117,5 +117,5 @@ def health_check():
 
 if __name__ == '__main__':
     # Production için threaded=True kullan (daha hızlı)
-    app.run(debug=False, host='0.0.0.0', port=5000, threaded=True)
+    app.run(debug=False, host='0.0.0.0', port=5001, threaded=True)
 
